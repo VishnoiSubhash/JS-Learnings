d2599f9c3eec8b6f5eb91f8fdee99822fee51143
VishnoiSubhash <114517966+VishnoiSubhash@users.noreply.github.com>
2024-11-24 13:24:18 +0000

    Datatypes

---
+++ 01_Basics/02_Datatype.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version 
+
+//alert(2+2) // we are using nodejs , not browser
+
+console.log(2+6);
+
+console.log("Vishnoi jii");
+
+
+let name = "Subhash"
+let age = 24
+let isLoggedIn = false
+let state;
+
+
+
+
+//number => 2 to power 53
+//bigint 
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined
+//symbol => unique
+
+
+//object
+console.log(typeof null); //object
+console.log(typeof undefined); // undefined
