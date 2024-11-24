8422c519521742ff20403cf5d82922ab8333f37c
VishnoiSubhash <114517966+VishnoiSubhash@users.noreply.github.com>
2024-11-24 12:07:31 +0000

    chapter-01(Variables)

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 112911 
+let accountEmail ="subhash@gmail.com"
+var accountPassword = "321123"
+let accountState;
+
+accountCity = "Jodhpur"
+
+//accountId = 5
+accountEmail ="vishsubhash@gmail.com"
+accountPassword ="231231"
+accountCity = "Karnataka"
+
+console.log(accountId);
+
+console.table([accountId,accountPassword, accountCity,accountState ,accountEmail]);
